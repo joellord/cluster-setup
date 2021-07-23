59675f5838f0d5c5983dcd6b4d92595f2783d26d
Joel Lord <joelphy@gmail.com>
2021-07-23 14:13:19 -0400

    Linux script

---
+++ linux.sh
@@ -0,0 +1,10 @@
+# Prepare the environment
+mkdir db0 && mkdir db1 && mkdir db2
+
+# Start your three instances of mongod
+mongod --port 27017 --dbpath $(pwd)/db0 --replSet myReplicaSet --bind_ip localhost --fork --logpath $(pwd)/log0
+mongod --port 27018 --dbpath $(pwd)/db1 --replSet myReplicaSet --bind_ip localhost --fork --logpath $(pwd)/log1
+mongod --port 27019 --dbpath $(pwd)/db2 --replSet myReplicaSet --bind_ip localhost --fork --logpath $(pwd)/log2
+
+# Login to your cluster to run the rs.initiate() command
+mongosh 
